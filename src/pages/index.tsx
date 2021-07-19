9c41c589082ddd35e6b213e813eab0b095881006
rodrigodasilva <ribeiro.rodrigodasilva>
2021-07-19 19:36:33 -0300

    Estilos globais do app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 
-import styles from '../styles/home.module.scss'
+
 
 export default function Home() {
   return (
@@ -9,7 +9,7 @@ export default function Home() {
         <title>Inicio | ig.news</title>
       </Head> 
 
-      <h1 className={styles.title}>
+      <h1>
         Hello 
         <span>
           world
